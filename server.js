637c282443ec255aae8b5a63de0e4fddb62ce808
Ronaiza Card <ronaizacard@gmail.com>
2016-11-06 18:15:21 -0200

    API route to serve json

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const Parser = require('./lib/Parser');
+
+const app = express();
+const PORT = 6969;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/games', (req, res) => {
+  fs.readFile('games.log', (err, logData) => {
+    if(err) {
+      console.log(err);
+      res.status(500).send({error : err });
+    }
+
+    let text = logData.toString();
+    let myParser = new Parser();
+    let result = myParser.parse(text);
+
+    res.json(result);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Express Server is listening on port: ' + PORT);
+});
