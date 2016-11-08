3f1d20cb604cc4049ce7c729111b6610c2fdbd7c
Ronaiza Card <ronaizacard@gmail.com>
2016-11-07 23:13:26 -0200

    adding total kills test

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -1,15 +1,53 @@
-'use strict';
-
 const chai   = require('chai');
 const expect = chai.expect;
 const Parser = require('../lib/Parser');
+const fs = require('fs');
+
 
 describe('Parser', () => {
-  it('parse() should return a [] if a empty string is passed in', () => {
-    let myParser = new Parser();
-    console.log(myParser.parse(''));
-    expect(myParser.parse('')).to.be.empty;
-  });
+  it('parse() should return [] if empty string is passed in', () => {
 
-  
+    let myParser = new Parser();
+
+    console.log(myParser.parse(''));
+
+    expect(myParser.parse('')).to.be.empty;
+
+  });
+});
+
+describe('Parser', () => {
+  it('parse() should return a group of game data if game.log is passed in', (done) => {
+
+    fs.readFile('./games.log', (err, data) => {
+      if ( err ) throw err;
+
+      let myParser = new Parser();
+      let logGame = data.toString();
+
+      console.log( myParser.parse ( logGame ) );;
+
+      expect( myParser.parse ( logGame ) ).to.be.a('array');
+      done();
+
+    });
+  });
+});
+
+describe('Parser', () => {
+  it('parse() should return total_kills', (done) => {
+
+    fs.readFile('./games.log', (err, logData) => {
+      if ( err ) throw err;
+
+      let text     = logData.toString();
+      let myParser = new Parser();
+      let result   = myParser.parse(text);
+
+      expect( ( result[0].game.total_kills ) ).to.be.a('number');
+
+      done();
+
+    });
+  });
 });
